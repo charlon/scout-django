b8b1cd3bab98b07fb14a2f4a052bc447780a265b
Charlon Palacay <charlon@gmail.com>
2017-11-08 19:18:04 -0800

    Adds hello world template and view.

--- scoutproject/scout/views.py
+++ scoutproject/scout/views.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    template = loader.get_template('scout/index.html')
+    context = {
+        'hello': "hello scout",
+    }
+    return HttpResponse(template.render(context, request))
